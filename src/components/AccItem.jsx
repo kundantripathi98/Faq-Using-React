d5703f1d628cbc52164962586bde83ee94e1aa41
Kundan Tripathi <124955318+kundantripathi98@users.noreply.github.com>
2024-08-09 11:16:38 +0000

    updated

--- src/components/AccItem.jsx
+++ src/components/AccItem.jsx
@@ -1,12 +1,15 @@
-const AccItem = ({ num, title, text, curOpen, onOpen }) => {
+const AccItem = ({ num, title, children, curOpen, onOpen }) => {
   const isOpen = num === curOpen;
 
   return (
-    <div className={`item ${isOpen ? "open" : ""}`} onClick={() => onOpen(num)}>
+    <div
+      className={`item ${isOpen ? "open" : ""}`}
+      onClick={() => onOpen(isOpen ? null : num)}
+    >
       <div className="number">{num}</div>
       <div className="title">{title}</div>
       <div className="icon">{isOpen ? "-" : "+"}</div>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 };
